021a46e599d39ed36fa937c9ecb26ca2c625b130
Ritik1306 <ritik.kaushik06@gmail.com>
2020-03-28 22:30:06 +0530

    adding addLast in MyLinkedList

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -7,4 +7,28 @@
 package problem3.node;
 
 public class Node {
+	private Student data;
+	private Node next;
+
+	public Node(Student data){
+		this.data=data;
+		this.next=null;
+	}
+
+	public Student getData() {
+		return data;
+	}
+
+	public void setData(Student data) {
+		this.data = data;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
 }
+
