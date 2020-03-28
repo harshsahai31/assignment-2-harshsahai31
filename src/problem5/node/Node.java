8491b2e9dc3be4732e6b48e03bc705c2048a9728
Ritik1306 <ritik.kaushik06@gmail.com>
2020-03-28 23:05:42 +0530

    problem done

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -7,4 +7,30 @@
 package problem5.node;
 // to define node properties
 public class Node {
+	private Student data;
+	private Node next;
+
+	public Node(Student data){
+		this.data=data;
+		this.next=null;
+	}
+
+	public Student getData() {
+		return data;
+	}
+
+	public void setData(Student data) {
+		this.data = data;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
 }
+
+
+
